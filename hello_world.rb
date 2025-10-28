ba53091d42a725929d8fe73b18786b3fda7a58ee
AwesomeTornado <harleypeterson@gmail.com>
2025-10-28 11:04:34 -0500

    Learning

--- hello_world.rb
+++ hello_world.rb
@@ -1,3 +1,38 @@
 # frozen_string_literal: true
+# Written by Harley Peterson, Oct 2025
+# This file is made for me to explore the basics of Ruby
+# I will be "thinking out loud" and practicing simple syntax first.
+#
+# hashtag for comments
+# not sure what multiline comments are yet.
+#
 
 puts "Hello, world!"
+puts "Stands for put string maybe?"
+p "No parens for function calls, does this also mean no parens for conditionals?"
+
+if true
+  p "Yep"
+  p "No curly braces either, end to close scopes"
+end
+
+puts "Ahh, interesting, puts does a \"ToString\" operation on its params, while p simply prints the param with no formatting."
+puts "Explains the quotes on the earlier messages."
+
+x = 5
+p x
+puts x
+puts "Python style variable naming, no type specifier"
+
+x = "And dynamic typing for variables too, just like python"
+p x
+
+for i in 0..5
+  y = 5 - i
+  puts String(y) + " bottles of beer on the wall, b" + y.to_s(2) + " bottles of beer."
+  puts "Take one down, pass it around... "
+end
+puts "Multiple parameters to puts are not concatenated, rather printed individually"
+puts "Very similar to python, for loop syntax, typecasting syntax, probably more."
+
+puts "Some powerful loop syntax now that I'm researching it online."
